76a53d913b32438fbdf6d0a2ee9d7a8c410bccee
liuwei <liuwei9502@163.com>
2017-12-15 18:08:07 +0800

    去掉adActivity 界面的360广告

--- app/src/main/java/com/zhaoyao/miaomiao/activity/AdActivity.java
+++ app/src/main/java/com/zhaoyao/miaomiao/activity/AdActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.AppCompatSpinner;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
@@ -24,8 +23,6 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.mediav.ads.sdk.adcore.Mvad;
-import com.mediav.ads.sdk.interfaces.IMvAdEventListener;
-import com.mediav.ads.sdk.interfaces.IMvBannerAd;
 import com.qq.e.ads.banner.ADSize;
 import com.qq.e.ads.banner.AbstractBannerADListener;
 import com.qq.e.ads.banner.BannerView;
@@ -160,46 +157,46 @@ public class AdActivity extends AppCompatActivity implements
             adView.loadAd(adRequest);
         }
 
-        Add360Ad1();
-        for (IMvBannerAd iMvBannerAd : listIMvBannerAd360) {
-            iMvBannerAd.setAdEventListener(new IMvAdEventListener() {
-                @Override
-                public void onAdviewGotAdSucceed() {
-                    LogUtils.d("IMvBannerAd\tonAdviewGotAdSucceed");
-                }
-
-                @Override
-                public void onAdviewGotAdFail() {
-                    LogUtils.d("IMvBannerAd\tonAdviewGotAdFail");
-                }
-
-                @Override
-                public void onAdviewIntoLandpage() {
-                    LogUtils.d("IMvBannerAd\tonAdviewIntoLandpage");
-                }
-
-                @Override
-                public void onAdviewDismissedLandpage() {
-                    LogUtils.d("IMvBannerAd\tonAdviewDismissedLandpage");
-                }
-
-                @Override
-                public void onAdviewClicked() {
-                    LogUtils.d("IMvBannerAd\tonAdviewClicked");
-                }
-
-                @Override
-                public void onAdviewClosed() {
-                    LogUtils.d("IMvBannerAd\tonAdviewClosed");
-                }
-
-                @Override
-                public void onAdviewDestroyed() {
-                    LogUtils.d("IMvBannerAd\tonAdviewDestroyed");
-                }
-            });
-            iMvBannerAd.showAds(this);
-        }
+//        Add360Ad1();
+//        for (IMvBannerAd iMvBannerAd : listIMvBannerAd360) {
+//            iMvBannerAd.setAdEventListener(new IMvAdEventListener() {
+//                @Override
+//                public void onAdviewGotAdSucceed() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewGotAdSucceed");
+//                }
+//
+//                @Override
+//                public void onAdviewGotAdFail() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewGotAdFail");
+//                }
+//
+//                @Override
+//                public void onAdviewIntoLandpage() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewIntoLandpage");
+//                }
+//
+//                @Override
+//                public void onAdviewDismissedLandpage() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewDismissedLandpage");
+//                }
+//
+//                @Override
+//                public void onAdviewClicked() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewClicked");
+//                }
+//
+//                @Override
+//                public void onAdviewClosed() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewClosed");
+//                }
+//
+//                @Override
+//                public void onAdviewDestroyed() {
+//                    LogUtils.d("IMvBannerAd\tonAdviewDestroyed");
+//                }
+//            });
+//            iMvBannerAd.showAds(this);
+//        }
 
 
         mTvBrushInterstitialSwitch = (TextView) findViewById(R.id.tvBrushInterstitialSwitch);
@@ -310,42 +307,42 @@ public class AdActivity extends AppCompatActivity implements
         }
     }
 
-    private void Add360Ad1() {
-        LinearLayout ll_360 = (LinearLayout) findViewById(R.id.ll_360);
-        for (int i = 0; i < ll_360.getChildCount(); i++) {
-            View childAt = ll_360.getChildAt(i);
-            if (childAt instanceof ViewGroup) {
-                IMvBannerAd bannerad = null;
-                if (i == 0) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID, false);
-                } else if (i == 1) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID2, false);
-                } else if (i == 2) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID3, false);
-                } else if (i == 3) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID4, false);
-                } else if (i == 4) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID5, false);
-                } else if (i == 5) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID6, false);
-                } else if (i == 6) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID7, false);
-                } else if (i == 7) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID8, false);
-                } else if (i == 8) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID9, false);
-                } else if (i == 9) {
-                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID10, false);
-                }
-                if (bannerad != null)
-                    listIMvBannerAd360.add(bannerad);
-            }
-        }
-    }
+//    private void Add360Ad1() {
+//        LinearLayout ll_360 = (LinearLayout) findViewById(R.id.ll_360);
+//        for (int i = 0; i < ll_360.getChildCount(); i++) {
+//            View childAt = ll_360.getChildAt(i);
+//            if (childAt instanceof ViewGroup) {
+//                IMvBannerAd bannerad = null;
+//                if (i == 0) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID, false);
+//                } else if (i == 1) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID2, false);
+//                } else if (i == 2) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID3, false);
+//                } else if (i == 3) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID4, false);
+//                } else if (i == 4) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID5, false);
+//                } else if (i == 5) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID6, false);
+//                } else if (i == 6) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID7, false);
+//                } else if (i == 7) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID8, false);
+//                } else if (i == 8) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID9, false);
+//                } else if (i == 9) {
+//                    bannerad = Mvad.showBanner((ViewGroup) childAt, this, Constants.Banner360ID10, false);
+//                }
+//                if (bannerad != null)
+//                    listIMvBannerAd360.add(bannerad);
+//            }
+//        }
+//    }
 
     private List<BannerView> listAdGdt = new ArrayList<>();
     private List<AdView> listAdViewGoogle = new ArrayList<>();
-    private List<IMvBannerAd> listIMvBannerAd360 = new ArrayList<>();
+//    private List<IMvBannerAd> listIMvBannerAd360 = new ArrayList<>();
 
     private BannerView initBanner(String BannerPosID) {
         BannerView mBannerView = new BannerView(this, ADSize.BANNER, Constants.APPID, BannerPosID);
@@ -377,14 +374,14 @@ public class AdActivity extends AppCompatActivity implements
         }
     }
 
-    /**
-     * 360横幅关闭
-     */
-    private void doCloseIMvBannerAdBanner() {
-        for (IMvBannerAd bannerView : listIMvBannerAd360) {
-            bannerView.closeAds();
-        }
-    }
+//    /**
+//     * 360横幅关闭
+//     */
+//    private void doCloseIMvBannerAdBanner() {
+//        for (IMvBannerAd bannerView : listIMvBannerAd360) {
+//            bannerView.closeAds();
+//        }
+//    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -426,7 +423,7 @@ public class AdActivity extends AppCompatActivity implements
             }
         }
         doCloseBanner();
-        doCloseIMvBannerAdBanner();
+//        doCloseIMvBannerAdBanner();
         listAdGdt.clear();
         closeAsPopup();
         mHandler.removeCallbacksAndMessages(null);
